dd537444159a38e12a5870a1e2cf87b7bee2a7a0
DEEPAK <130702851+deepak4728@users.noreply.github.com>
2023-08-29 00:41:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// let n1=prompt("please enter your name- ");
+// const student={
+//     name:"deepak",
+//     class:"BE",
+//     marks:100,
+//     sum:()=>{
+//         console.log("its sums function executed.");
+//     }
+// };
+// student.name="dee";
+// function nam(){
+//     console.log(`its ok ${student.name}, no tension.`);
+
+// }
+
+// console.log(student);
+// console.log(student.name);
+// console.log(student.marks);
+// console.log(student.sum());
+
+const person={
+    name:"Deepak",
+    class:"BE",
+};
+console.log(person.name);
+const por=new Person();
+console.log(por);
+console.log(por.name);
+
